38d8f3d0b4efad3e93151d9ed483335a5e86ff96
FranciscoJChCano <franciscojcharnecocano@gmail.com>
2024-06-21 11:25:40 +0200

    add some code on war class

--- src/main/java/org/example/war/War.java
+++ src/main/java/org/example/war/War.java
@@ -1,4 +1,43 @@
 package org.example.war;
 
+import java.util.ArrayList;
+
+import org.example.saxon.Saxon;
+import org.example.vikins.Viking;
+
+
 public class War {
+
+private ArrayList<Viking> vikingArmy;
+private ArrayList<Saxon> saxonArmy;
+
+public War(){
+    this.vikingArmy = new ArrayList<>();
+    this.saxonArmy = new ArrayList<>();
+}
+
+public void addViking(Viking viking){
+ this.vikingArmy.add(viking);
+
+
+}
+public void addSaxon(Saxon saxon){
+    this.saxonArmy.add(saxon);
+   
+}
+
+   public String vikingAttack(){
+    return "";
+   
+   }
+   public String saxonAttack(){
+    return "";
+   
+   }
+   public String showStatus(){
+    return "";
+   
+   }
+
+
 }
